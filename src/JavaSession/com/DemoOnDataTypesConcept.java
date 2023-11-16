6b5f127b7d65d9e6f2f8fb4e747e315ca939c264
NamrathaRM01 <namratharm01@gmail.com>
2023-11-16 11:42:55 +0530

    first commit

---
+++ src/JavaSession/com/DemoOnDataTypesConcept.java
@@ -0,0 +1,10 @@
+package JavaSession.com;
+
+public class DemoOnDataTypesConcept {
+
+	public static void main(String[] args) {
+	System.out.println("Hello");	// TODO Auto-generated method stub
+
+	}
+
+}
