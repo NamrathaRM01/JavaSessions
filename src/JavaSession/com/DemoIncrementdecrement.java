30d2e87bb0e2c3a23191a46ee74524870a084bd6
NamrathaRM01 <namratharm01@gmail.com>
2023-11-21 19:20:17 +0530

    Increment&decrement

---
+++ src/JavaSession/com/DemoIncrementdecrement.java
@@ -0,0 +1,33 @@
+package JavaSession.com;
+
+public class DemoIncrementdecrement {
+
+	public static void main(String[] args) {
+	
+		//Post Increment
+		int i=2;
+		int j=i++;
+		System.out.println(i);
+		System.out.println(j);
+		
+		//Post Decrement
+		int ii=4;
+		int jj=i--;
+		System.out.println(ii);
+		System.out.println(jj);
+		
+		//Pre Increment
+		int a=2;
+		int b=++a;
+		System.out.println(a);
+		System.out.println(b);
+		
+		//Pre Decrement
+		int e=2;
+		int f=--e;
+		System.out.println(e);
+		System.out.println(f);
+
+	}
+
+}
